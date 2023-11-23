9a7821893a5d17406a336132cc65cb867b190335
chaehaeun <chae_3210@naver.com>
2023-11-23 23:06:28 +0900

    [Add] 15-4 SSG 적용하기 3
    fallback blocking, true 실습

--- pages/country/[code].jsx
+++ pages/country/[code].jsx
@@ -6,6 +6,13 @@ const Country = ({ country }) => {
   const router = useRouter();
   const { code } = router.query;
 
+  if (router.isFallback) {
+    // 라우터 객체로 현재 fallback 상태를 확인할 수 있음
+    return <div>Loading...</div>;
+  }
+
+  if (!country) return <div>존재하지 않는 국가입니다.</div>;
+
   return (
     <div>
       {country.commonName} {country.officialName}
@@ -33,7 +40,10 @@ export const getStaticPaths = async () => {
         },
       },
     ],
-    fallback: false, // fallback은 불러올 경로가 없을 때 어떻게 처리할지 설정하는 옵션 : false는 404 페이지를 반환
+    fallback: true,
+    // fallback은 불러올 경로가 없을 때 어떻게 처리할지 설정하는 옵션 : false는 404 페이지를 반환
+    // blocking : 빌드 시점에 없는 경로를 미리 생성하지 않고, 요청이 들어올 때 서버사이드에서 생성. 사용자는 완성된 페이지가 로드될 때까지 대기해야됨. 이후 로드 때는 정적 페이지로 캐싱
+    // true : 요청이 들어오면 빠르게 빈 페이지를 반환하고, 클라이언트 사이드에서 페이지 생성. 이후 로드 때는 정적 페이지로 캐싱
   };
 };
 
